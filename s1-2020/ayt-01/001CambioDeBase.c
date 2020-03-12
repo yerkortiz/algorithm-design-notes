269802437b9805b9add457500935c5f25a91eda2
Yerko <yerko.ortizm@mail.udp.cl>
2020-03-11 22:32:37 -0300

    cmt #4
    cambios menores

---
+++ s1-2020/ayt-01/001CambioDeBase.c
@@ -0,0 +1,34 @@
+/* 
+    Author: Yerko Ortiz
+    Código para transformar un número natural n de 
+    base 10, en un número natural de base k.
+*/
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef unsigned long long int uint64_t;
+
+#define EXIT_PROGRAM return 0
+/* base 10 a base k */
+void solution(uint64_t n, uint64_t k)
+{
+    if(n > 1) solution(n / k, k);
+    printf("%llu ", n % k);
+}
+/* decimal a binario */
+void bin(uint64_t n)
+{
+    if(n > 1) bin(n >> 1);
+    printf("%llu ", n & 1);
+}
+int main(int argc, char **argv)
+{
+    uint64_t n, k;
+    //scanf("%llU %llU", &n, &k);
+    if(argc != 3) EXIT_PROGRAM;
+    n = atoi(argv[1]);
+    k = atoi(argv[2]);
+    if(k < 2) EXIT_PROGRAM;
+    solution(n, k);
+    EXIT_PROGRAM;
+}
