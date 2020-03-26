ec56d8f06fde5008088b91c150a819a228712616
Yerko <yerko.ortizm@mail.udp.cl>
2020-03-26 19:30:03 -0300

    tarea-01
    solucion

--- tarea-01/solution.java
+++ tarea-01/solution.java
@@ -1,11 +1,24 @@
 import java.util.Scanner;
-class tarea {
-    public static void main 
+class Tarea {
+    static int tarea(int ps[], int dps[], int n)
+    {
+        int sum = 0;
+        for(int i = 0; i < n; ++i)
+            for(int j = i; j < n; ++j) 
+                sum += (dps[j] * ps[i]);
+        return sum;
+    }
+    public static void main (String[] args)
     {
         Scanner scanner = new Scanner(System.in);
         int n = scanner.nextInt();
-        int[] arr = new int[n];
+        int[] dps = new int[n];
+        int[] ps = new int[n];
         for(int i = 0; i < n; ++i)
-            arr[i] = scanner.nextInt();
+            dps[i] = scanner.nextInt();
+        for(int i = 0; i < n; ++i)
+            ps[i] = scanner.nextInt();
+        scanner.close();
+        System.out.println(tarea(ps, dps, n));
     }
 }
