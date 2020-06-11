9b331735dda4d53fc5c508adf21f527fcd961113
Yerko <yerko.ortizm@mail.udp.cl>
2020-06-11 09:41:08 -0400

    actualizacion

--- tut-01/cola.java
+++ tut-01/cola.java
@@ -33,28 +33,5 @@ class Main {
         }
         System.out.println(cola1.peek());
         cola1.poll();
-
-
-
-
-
-
-
-
-
-
-        //Queue<Integer> numbers = new LinkedList<>();
-
-        /*
-        numbers.offer(1);
-        numbers.offer(2);
-        numbers.offer(3);
-        System.out.println("Queue: " + numbers);
-        int accessedNumber = numbers.peek();
-        System.out.println("Accessed Element: " + accessedNumber);
-        int removedNumber = numbers.poll();
-        System.out.println("Removed Element: " + removedNumber);
-        System.out.println("Updated Queue: " + numbers);
-        */
     }
 }
