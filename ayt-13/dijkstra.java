8192a8887a3bb613faead1c0c79b77d186d85e98
Yerko <yerko.ortizm@mail.udp.cl>
2020-06-23 12:29:38 -0400

    dijkstra & prim

---
+++ ayt-13/dijkstra.java
@@ -0,0 +1,57 @@
+import java.util.*;
+
+public class Dijkstra {
+    public static void main(String[] args) {
+        Scanner stdin = new Scanner(System.in);
+        int V = stdin.nextInt(), 
+            E = stdin.nextInt();
+        int[][] adj_mtrx = new int[V][V];//grafo con pesos
+        for (int[] row: adj_mtrx) 
+            Arrays.fill(row, 0);
+        for(int i = 0; i < E; ++i) {//grafo no dirigido
+            int u = stdin.nextInt(),
+                v = stdin.nextInt(),
+                w = stdin.nextInt();
+            adj_mtrx[u - 1][v - 1] = w;
+            adj_mtrx[v - 1][u - 1] = w;
+        }
+        int[] distance = new int[V];
+        PriorityQueue<Node> pq = new PriorityQueue<Node>(V, new Node());
+        boolean[] visited = new boolean[V];
+        /* Incializar las variables */ 
+        int source = 0; //origen
+        int inf = ((1 << 30) | ((1 << 30) - 1));//valor maximo de int de 32 bits en complemento a 2
+        Arrays.fill(distance, inf);
+
+        /*algoritmo de dijkstra*/
+        pq.add(new Node(source, 0));
+        distance[source] = 0;
+
+        while(!pq.isEmpty()) {
+            int a = pq.peek().node; pq.poll();
+            if(visited[a] == true) continue;
+            for(int d = 0; d < V; ++d) {
+                if(adj_mtrx[a][d] == 0) continue;
+                int w = adj_mtrx[a][d];
+                if(distance[a] + w < distance[d]) {
+                    distance[d] = distance[a] + w;
+                    pq.add(new Node(d, -distance[d]));
+                }
+            }
+        }
+        for(int d_: distance) System.out.print(d_ + " ");
+        }
+    } 
+public class Node implements Comparator<Node>{ 
+    public int node, cost; 
+    public Node(){} 
+    public Node(int node, int cost) { 
+        this.node = node; 
+        this.cost = cost; 
+    } 
+    public int compare(Node node1, Node node2) { 
+        if (node1.cost < node2.cost) return -1; 
+        if (node1.cost > node2.cost) return 1; 
+        return 0; 
+    } 
+}
---
+++ ayt-13/prim.java
@@ -0,0 +1,55 @@
+import java.util.*;
+public class Prim {
+    public static void main(String args[]) {
+        Scanner stdin = new Scanner(System.in);
+        int V = stdin.nextInt(), 
+            E = stdin.nextInt();
+        int[][] adj_mtrx = new int[V][V];//grafo con pesos
+        for (int[] row: adj_mtrx) 
+            Arrays.fill(row, 0);
+        for(int i = 0; i < E; ++i) {//grafo no dirigido
+            int u = stdin.nextInt(),
+                v = stdin.nextInt(),
+                w = stdin.nextInt();
+            adj_mtrx[u - 1][v - 1] = w;
+            adj_mtrx[v - 1][u - 1] = w;
+        }
+        //algoritmo de prim
+        boolean[] visited = new boolean[V];
+        PriorityQueue<Node> pq = new PriorityQueue<Node>(V, new Node());
+        visited[0] = true;
+        for(int i = 0; i < V; ++i) {
+            if(adj_mtrx[0][i] == 0) continue;
+            int v = adj_mtrx[0][i];
+            if(visited[i] == false) pq.add(new Node(i, v));
+        }
+        int mst_cost = 0;
+        while(!pq.isEmpty()) {
+            Node front = pq.peek(); pq.poll();
+            int u = front.node, w = front.cost;
+            if(visited[u] == false) {
+                mst_cost += w;
+                visited[u] = true;
+                for(int i = 0; i < V; ++i) {
+                    if(adj_mtrx[u][i] == 0) continue;
+                    int v = adj_mtrx[u][i];
+                    if(visited[i] == false) pq.add(new Node(i, v));
+                }
+            }
+        }
+        System.out.println(mst_cost);
+    }
+}
+public class Node implements Comparator<Node>{ 
+    public int node, cost; 
+    public Node(){} 
+    public Node(int node, int cost) { 
+        this.node = node;
+        this.cost = cost; 
+    } 
+    public int compare(Node node1, Node node2) { 
+        if (node1.cost < node2.cost) return -1; 
+        if (node1.cost > node2.cost) return 1; 
+        return 0; 
+    } 
+}
